e74d916cf00446ed04e753f7234b6c3f64ec291e
VanilCitatka <VanilCItatka@gmail.com>
2022-11-22 23:33:29 +0300

    full bot

--- handlers/parsing_chat.py
+++ handlers/parsing_chat.py
@@ -1,17 +1,42 @@
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.filters import Command, Text
-
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import CallbackQuery, ReplyKeyboardRemove
 from keyboard import create_main_keyboard
 
 parse = Router()
 
+categories = ['Часы', 'Кроссовки', 'Cумки', 'Одежда']
+
+
+class WhatToParse(StatesGroup):
+    category = State()
+    date = State()
+
 
 @parse.message(Command(commands=["start"]))
-async def bot_start(msg: types.Message):
-    await msg.answer('Привет пошёл нахуй', reply_markup=create_main_keyboard())
+async def bot_start(msg: types.Message, state: FSMContext):
+    await msg.answer('Привет! Что парсим?', reply_markup=create_main_keyboard())
+    await state.set_state(WhatToParse.category)
 
 
 # Работа с текстом
-@parse.message(Text(text=['Обувь', 'Кроссовки', 'Одежда', 'Сумки']))
-async def text_test(msg: types.Message):
-    await msg.answer('Пошёл нахуй со своими ' + msg.text, reply_markup=create_main_keyboard())
+@parse.message(WhatToParse.category, F.text.in_(categories))
+async def data_choose(msg: types.Message, state: FSMContext):
+    await state.update_data(category_type=msg.text.lower())
+    await msg.answer('Введите дату в формате гггг-мм-дд', reply_markup=ReplyKeyboardRemove())
+    await state.set_state(WhatToParse.date)
+
+
+@parse.message(WhatToParse.date)
+async def fin(msg: types.Message, state: FSMContext):
+    hui = await state.get_data()
+    await msg.answer(f'Дата: {msg.text}, Категория: {hui["category_type"]}')
+    await msg.answer('И ДАЛЬШЕ Я ПОДКЛЮЧАЮ СКРИПТ ЗАРАНЕЕ ИДИ НАХУЙ ПЕСОК Я ПРОГРАММИСТ Я ТАК ВИЖУ')
+    await state.clear()
+
+
+
+
+
--- keyboard.py
+++ keyboard.py
@@ -3,33 +3,12 @@ from aiogram import types
 
 kb = [
     [
-        types.KeyboardButton(text='Обувь'),
+        types.KeyboardButton(text='Часы'),
         types.KeyboardButton(text='Кроссовки'),
         types.KeyboardButton(text='Одежда'),
         types.KeyboardButton(text='Сумки')
     ]
 ]
-
-
-def create_year_kb():
-    year_kb = [[types.KeyboardButton(text=str(year)) for year in range(2020, 2026)]]
-    builder = ReplyKeyboardBuilder(year_kb)
-    builder.adjust(3)
-    return builder.as_markup()
-
-
-def create_month_kb():
-    month_kb = [[types.KeyboardButton(text=str(month)) for month in range(1, 13)]]
-    builder = ReplyKeyboardBuilder(month_kb)
-    builder.adjust(4)
-    return builder.as_markup()
-
-
-def create_day_kb():
-    day_kb = [[types.KeyboardButton(text=str(day)) for day in range(1, 32)]]
-    builder = ReplyKeyboardBuilder(day_kb)
-    builder.adjust(8)
-    return builder.as_markup()
 
 
 def create_main_keyboard():
